1836dec777d7c3031d7ab994d14b6e261104d494
MS Khan <saabir.khan18@gmail.com>
2022-06-09 19:44:45 +0530

    test

--- JwtAuth/Controllers/NameController.cs
+++ JwtAuth/Controllers/NameController.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using JwtAuth.Models;
+using Microsoft.AspNetCore.Authorization;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace JwtAuth.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class NameController : ControllerBase
@@ -25,6 +27,7 @@ namespace JwtAuth.Controllers
             return new string[] { "value1", "value2" };
         }
 
+        [AllowAnonymous]
         [HttpPost("auth")]
         public IActionResult Authenticate([FromBody] UserCred userCred)
         {
